3fdd661d01fbbb01a79c5b1db27aa5afd1db8dc8
ehsan <ehsan2003.2003.382@gmail.com>
2022-04-09 12:49:04 +0430

    reformats the code

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ example :
 ```typescript
 const pool = new Pool(5);
 const urls = [
-    "https://www.google.com/1",
-    "https://www.google.com/2",
-    "https://www.google.com/3",
-    "https://www.google.com/4",
-    "https://www.google.com/5",
-    "https://www.google.com/6",
-    "https://www.google.com/7",
+  "https://www.google.com/1",
+  "https://www.google.com/2",
+  "https://www.google.com/3",
+  "https://www.google.com/4",
+  "https://www.google.com/5",
+  "https://www.google.com/6",
+  "https://www.google.com/7",
 ];
 // here maximum concurrent url being fetched is 5
 await Promise.all(urls.map((u) => pool.exec(() => fetch(u))));
@@ -24,4 +24,5 @@ await Promise.all(urls.map((u) => pool.exec(() => fetch(u))));
 
 https://doc.deno.land/https://raw.githubusercontent.com/ehsan2003/task-pool/main/mod.ts
 
-the code is simple so if you confused about anything read the code its most of the time best way to understand things or just open an issue
+the code is simple so if you confused about anything read the code its most of
+the time best way to understand things or just open an issue
--- mod.ts
+++ mod.ts
@@ -2,14 +2,14 @@ type Task<T> = () => Promise<T>;
 import Queue from "https://esm.sh/yocto-queue@1.0.0";
 
 type DetachedPromise<T> = {
-    promiseResolve: (value: T | PromiseLike<T>) => void;
-    promiseReject: (value: T | PromiseLike<T>) => void;
-    promise: Promise<T>;
+  promiseResolve: (value: T | PromiseLike<T>) => void;
+  promiseReject: (value: T | PromiseLike<T>) => void;
+  promise: Promise<T>;
 };
 /**
  * a pool of tasks ensuring maximum concurrent tasks running is less than the size
  * @param size maximum concurrent tasks
- * 
+ *
  * @example
  * const pool = new Pool(5);
  * const urls = [
@@ -25,88 +25,88 @@ type DetachedPromise<T> = {
  * await Promise.all(urls.map((u) => pool.exec(() => fetch(u))));
  */
 export class Pool {
-    private workingCount = 0;
-    private waitQueue: Queue<DetachedPromise<void>> = new Queue();
-    constructor(private size: number) {
-        if (size < 1) {
-            throw new Error("Pool size must be greater than 0");
-        }
+  private workingCount = 0;
+  private waitQueue: Queue<DetachedPromise<void>> = new Queue();
+  constructor(private size: number) {
+    if (size < 1) {
+      throw new Error("Pool size must be greater than 0");
     }
-    /**
-     * execute a task in pool
-     * @param task task to run
-     * @returns promise that resolves when the task is done or rejects if task rejects
-     */
-    public async exec<T>(task: Task<T>): Promise<T> {
-        const detachedPromise = this.createPromise<T>();
-        if (this.workingCount < this.size) {
-            this.run(task, detachedPromise);
-        } else {
-            const p = this.createPromise<void>();
-            this.waitQueue.enqueue(p);
-            await p.promise;
-            this.run<T>(task, detachedPromise);
-        }
-
-        return detachedPromise.promise;
+  }
+  /**
+   * execute a task in pool
+   * @param task task to run
+   * @returns promise that resolves when the task is done or rejects if task rejects
+   */
+  public async exec<T>(task: Task<T>): Promise<T> {
+    const detachedPromise = this.createPromise<T>();
+    if (this.workingCount < this.size) {
+      this.run(task, detachedPromise);
+    } else {
+      const p = this.createPromise<void>();
+      this.waitQueue.enqueue(p);
+      await p.promise;
+      this.run<T>(task, detachedPromise);
     }
 
-    private run<T>(
-        task: Task<T>,
-        { promiseResolve, promiseReject }: DetachedPromise<T>
-    ) {
-        this.increaseExecuting();
-        task()
-            .then((result) => {
-                this.decreaseExecuting();
-                promiseResolve(result);
-            })
-            .catch((e) => {
-                this.decreaseExecuting();
-                promiseReject(e);
-            });
+    return detachedPromise.promise;
+  }
+
+  private run<T>(
+    task: Task<T>,
+    { promiseResolve, promiseReject }: DetachedPromise<T>,
+  ) {
+    this.increaseExecuting();
+    task()
+      .then((result) => {
+        this.decreaseExecuting();
+        promiseResolve(result);
+      })
+      .catch((e) => {
+        this.decreaseExecuting();
+        promiseReject(e);
+      });
+  }
+  /**
+   * returns the size passed on constructor ( maximum concurrent tasks )
+   * @returns
+   */
+  public getSize(): number {
+    return this.size;
+  }
+  /**
+   * returns the number of working tasks
+   * @returns number of working tasks
+   */
+  public getWorkingCount(): number {
+    return this.workingCount;
+  }
+  /**
+   * returns the number of pending tasks
+   * @returns number of pending tasks
+   */
+  public getPendingCount(): number {
+    return this.waitQueue.size;
+  }
+  private increaseExecuting() {
+    this.workingCount++;
+  }
+  private decreaseExecuting() {
+    this.workingCount--;
+    if (this.workingCount < this.size) {
+      this.resolveFirst();
     }
-    /**
-     * returns the size passed on constructor ( maximum concurrent tasks )
-     * @returns
-     */
-    public getSize(): number {
-        return this.size;
-    }
-    /**
-     * returns the number of working tasks
-     * @returns number of working tasks
-     */
-    public getWorkingCount(): number {
-        return this.workingCount;
-    }
-    /**
-     * returns the number of pending tasks
-     * @returns number of pending tasks
-     */
-    public getPendingCount(): number {
-        return this.waitQueue.size;
-    }
-    private increaseExecuting() {
-        this.workingCount++;
-    }
-    private decreaseExecuting() {
-        this.workingCount--;
-        if (this.workingCount < this.size) {
-            this.resolveFirst();
-        }
-    }
-    private resolveFirst() {
-        this.waitQueue.dequeue()?.promiseResolve();
-    }
-    private createPromise<T>(): DetachedPromise<T> {
-        let promiseResolve;
-        let promiseReject;
-        const promise = new Promise<T>((resolve, reject) => {
-            promiseResolve = resolve;
-            promiseReject = reject;
-        });
-        // deno-lint-ignore no-explicit-any
-        return { promiseResolve, promiseReject, promise } as any;
-    }
+  }
+  private resolveFirst() {
+    this.waitQueue.dequeue()?.promiseResolve();
+  }
+  private createPromise<T>(): DetachedPromise<T> {
+    let promiseResolve;
+    let promiseReject;
+    const promise = new Promise<T>((resolve, reject) => {
+      promiseResolve = resolve;
+      promiseReject = reject;
+    });
+    // deno-lint-ignore no-explicit-any
+    return { promiseResolve, promiseReject, promise } as any;
+  }
 }
